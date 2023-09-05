1d5476ab37513292cd09ec98b01fcbd5cfb7db6e
annabelliard <annabelliard@gmail.com>
2023-09-05 20:11:42 +0200

    better implementation of min/max quality

--- Kotlin/src/main/kotlin/com/gildedrose/model/BaseItem.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/model/BaseItem.kt
@@ -4,27 +4,30 @@ import com.gildedrose.config.QualityRequirements
 
 open class BaseItem(name: String, sellIn: Int, quality: Int): Item(name, sellIn, quality) {
 
-    open val maxQuality = QualityRequirements.MAX_QUALITY
-    open val minQuality = QualityRequirements.MIN_QUALITY
-
     fun update() {
         updateQuality()
         checkMinMaxQuality()
         updateSellIn()
     }
 
+    open fun getMaxQuality(): Int {
+        return QualityRequirements.MAX_QUALITY
+    }
+
+    open fun getMinQuality(): Int {
+        return QualityRequirements.MIN_QUALITY
+    }
+
     fun checkMinMaxQuality() {
-        if(this.quality > maxQuality){
-            this.quality = maxQuality
-        }
-        if(this.quality < minQuality){
-            this.quality = minQuality
+        with(this){
+             if(quality > getMaxQuality()) { quality = getMaxQuality()}
+             if(quality < getMinQuality()) { quality = getMinQuality()}
         }
     }
 
     open fun updateQuality(){
         if(this.sellIn <= 0 ){
-            this.quality = this.quality - 2
+            this.quality = this.quality.minus(2)
         }
         else {
             this.quality --
--- Kotlin/src/main/kotlin/com/gildedrose/model/type/Sulfuras.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/model/type/Sulfuras.kt
@@ -3,15 +3,16 @@ package com.gildedrose.model.type
 import com.gildedrose.config.QualityRequirements
 import com.gildedrose.model.BaseItem
 
-class Sulfuras(name: String, sellIn: Int, quality: Int): BaseItem(name, sellIn, quality) {
+class Sulfuras(name: String, sellIn: Int, quality: Int) : BaseItem(name, sellIn, quality) {
+    override fun getMaxQuality(): Int {
+        return QualityRequirements.MAX_QUALITY_SULFURAS
+    }
 
-    override val maxQuality = QualityRequirements.MAX_QUALITY_SULFURAS
-
-    override fun updateQuality(){
+    override fun updateQuality() {
         //Do nothing as Sulfuras is a legendary item that cannot be decreased in quality
     }
 
-    override fun updateSellIn(){
+    override fun updateSellIn() {
         //Do nothing as Sulfuras is a legendary item that cannot be sold
     }
 }
